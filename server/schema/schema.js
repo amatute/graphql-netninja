7028f208b6b08795e8538257bc59a1891807fb74
Alejandro <alexghoro@outlook.com>
2019-12-04 14:29:35 -0400

    add schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,15 +5,26 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
-  GraphQLID
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
 
-var books = [
-  {name:'La Rebelion de Atlas', genre:'Novel', id:'1'},
-  {name:'Camino de Servidumbre', genre:'Economics', id:'2'},
-  {name:'User Story Mapping', genre:'Computer Science', id:'3'}
+const books = [
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+  {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+  {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
+]
+
+const authors =  [
+  {name: 'Patrick Rothfuss', age: 44, id:"1"},
+  {name: 'Brandon Sanderson', age: 42, id:"2"},
+  {name: 'Terry Pratchett', age: 66, id:"3"},
 ];
 
 const BookType = new GraphQLObjectType({
@@ -21,9 +32,31 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
-    genre: {type: GraphQLString}
+    genre: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, {id: parent.authorId});
+      }
+    }
   })
-})
+});
+
+const AuthorType = new GraphQLObjectType({
+  name:'Author',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, {authorId: parent.id})
+      }
+    }
+
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name:'RootQueryType',
@@ -32,8 +65,14 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args:{id:{type: GraphQLID}},
       resolve(parent, args){
-        // code to get data
         return _.find(books, {id: args.id});
+      }
+    },
+    author:{
+      type: AuthorType,
+      args:{id:{type: GraphQLID}},
+      resolve(parent, args){
+        return _.find(authors, {id: args.id});
       }
     }
   }
